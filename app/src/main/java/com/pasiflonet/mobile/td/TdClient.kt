ad965753bbe69f1fab85ca2783c8cd395cd71081
Termux User <termux@pasiflonet.com>
2026-01-06 19:48:22 +0200

    Fix: avoid kotlin.Function clash by fully-qualifying TdApi types

--- app/src/main/java/com/pasiflonet/mobile/td/TdClient.kt
+++ app/src/main/java/com/pasiflonet/mobile/td/TdClient.kt
@@ -3,14 +3,13 @@ package com.pasiflonet.mobile.td
 import android.content.Context
 import android.util.Log
 import org.drinkless.tdlib.Client
-import org.drinkless.tdlib.TdApi
 
 class TdClient(
     private val ctx: Context,
     private val apiId: Int,
     private val apiHash: String
 ) {
-    var onUpdate: ((TdApi.Object) -> Unit)? = null
+    var onUpdate: ((org.drinkless.tdlib.TdApi.Object) -> Unit)? = null
 
     private val client: Client = Client.create(
         Client.ResultHandler { obj -> onUpdate?.invoke(obj) },
@@ -18,12 +17,15 @@ class TdClient(
         Client.ExceptionHandler { e -> Log.e("TdClient", "TDLib fatal: ${e.message}", e) }
     )
 
-    fun send(query: TdApi.Function, cb: (TdApi.Object) -> Unit = {}) {
+    fun send(
+        query: org.drinkless.tdlib.TdApi.Function,
+        cb: (org.drinkless.tdlib.TdApi.Object) -> Unit = {}
+    ) {
         client.send(query, Client.ResultHandler { obj -> cb(obj) })
     }
 
     fun setTdlibParameters(databaseDir: String, filesDir: String) {
-        val req = TdApi.SetTdlibParameters(
+        val req = org.drinkless.tdlib.TdApi.SetTdlibParameters(
             false,
             databaseDir,
             filesDir,
