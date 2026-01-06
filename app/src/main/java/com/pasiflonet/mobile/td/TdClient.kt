dde33586099bffe5b93886bd6ebac3b5af558af3
Termux User <termux@pasiflonet.com>
2026-01-06 19:54:36 +0200

    Fix compile: TdApi.Function is generic in td-1.8.56 (use Function<*>)

--- app/src/main/java/com/pasiflonet/mobile/td/TdClient.kt
+++ app/src/main/java/com/pasiflonet/mobile/td/TdClient.kt
@@ -3,13 +3,14 @@ package com.pasiflonet.mobile.td
 import android.content.Context
 import android.util.Log
 import org.drinkless.tdlib.Client
+import org.drinkless.tdlib.TdApi
 
 class TdClient(
     private val ctx: Context,
     private val apiId: Int,
     private val apiHash: String
 ) {
-    var onUpdate: ((org.drinkless.tdlib.TdApi.Object) -> Unit)? = null
+    var onUpdate: ((TdApi.Object) -> Unit)? = null
 
     private val client: Client = Client.create(
         Client.ResultHandler { obj -> onUpdate?.invoke(obj) },
@@ -17,15 +18,13 @@ class TdClient(
         Client.ExceptionHandler { e -> Log.e("TdClient", "TDLib fatal: ${e.message}", e) }
     )
 
-    fun send(
-        query: org.drinkless.tdlib.TdApi.Function,
-        cb: (org.drinkless.tdlib.TdApi.Object) -> Unit = {}
-    ) {
+    // NOTE: In td-1.8.56.aar, TdApi.Function is generic -> use Function<*>
+    fun send(query: TdApi.Function<*>, cb: (TdApi.Object) -> Unit = {}) {
         client.send(query, Client.ResultHandler { obj -> cb(obj) })
     }
 
     fun setTdlibParameters(databaseDir: String, filesDir: String) {
-        val req = org.drinkless.tdlib.TdApi.SetTdlibParameters(
+        val req = TdApi.SetTdlibParameters(
             false,
             databaseDir,
             filesDir,
